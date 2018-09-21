0f24793d41ba2c3c9b40bd97b3b68deca19e5622
Gleb S <glebright@gmail.com>
2018-09-21 19:47:18 +0300

    Initial Commit

---
+++ cameraML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cameraML
+//
+//  Created by Gleb Shendrik on 21/09/2018.
+//  Copyright © 2018 Gleb Shendrik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
